7b66f28fc07af6527b30b2060fb358181208b9c1
Sushant Rane <srane@microsoft.com>
2018-02-14 15:43:36 -0600

    RoleDefinitionUpdates

---
+++ RoleDefinitionTemplates/CreateRoleDefinitions.ps1
@@ -0,0 +1,18 @@
+﻿$templatesFilePath = "{location to the role definition templates}"
+$subscriptionId = "{subscriptionId}"
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+$templates = Get-ChildItem $templatesFilePath -Filter *.json
+foreach ($template in $templates)
+{
+    try
+    {
+        New-AzureRmRoleDefinition -InputFile "$templatesFilePath\$($template.Name)"
+    }
+
+    catch
+    {
+        $ErrorMessage = $_.Exception.Message
+        Write-Host "Unable to create Role Definition for $($template.Name) with Error: $ErrorMessage" -ForegroundColor Red
+    }
+}
